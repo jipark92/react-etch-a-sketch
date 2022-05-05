85856224e8c5de2d9324fa92a5c44f89f4ac0a13
Ji Park <jiyongprk@gmail.com>
2022-05-05 13:45:40 -0400

    work on background color change function and style

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -1,15 +1,26 @@
-import {useEffect} from 'react'
-import Buttons from './Buttons'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react'
 
 export default function Sketch() {
     
     let gridArrays = []
 
+    const [blackMarker, setBlackMarker] = useState(false)
+
+    const blah = () => {
+        
+        gridArrays.forEach((gridArray)=>{
+            console.log(gridArray)
+            gridArray.backgroundColor="black"
+        })
+    }
+
     const makeGrids = () => {
         for (let i=0;i<100;i++){
-            gridArrays.push(<div className='grids'></div>)
+            gridArrays.push(<div className='grids' onMouseOver={blackMarker? blah : blah}  ></div>)
         }
-        console.log(gridArrays)
+        // console.log(gridArrays)
 
         let showGrids = gridArrays.map((gridArray)=>{
             return (
@@ -21,17 +32,24 @@ export default function Sketch() {
         return showGrids
     }
 
-    // useEffect(()=>{
+    const activateBlackMarker = () => {
+        console.log('black on', blackMarker)
+        setBlackMarker((blackMarker)=> blackMarker = !blackMarker)
+    }
 
-    // })
-    
+
 
     return (
         <div className='sketch-container'>
             <div className='grids-container'>
                 {makeGrids()}
             </div>
-            <Buttons/>
+            <div className="btns-container">
+                <Button className='bg-dark' onClick={activateBlackMarker}>Black Marker</Button>
+                <Button className='bg-dark'>Eraser</Button>
+                <Button className='bg-dark'>Clear</Button>
+                <Button className='bg-dark'>Change Grid Size</Button>
+            </div>
         </div>
     )
 }
