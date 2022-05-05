c5b747b53526fc6ddd4393b47ca12cf48991883d
Ji Park <jiyongprk@gmail.com>
2022-05-05 16:34:54 -0400

    add about section and style

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,30 @@
+import {Accordion } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div className='about-container'>
+            <h1>ABOUT</h1>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Tools Used?</Accordion.Header>
+                    <Accordion.Body>
+                        <ul>
+                            <li>React</li>
+                            <li>Javascript</li>
+                            <li>Css</li>
+                            <li>Bootstrap</li>
+                        </ul>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Created By?</Accordion.Header>
+                    <Accordion.Body>
+                        <p>Ji Park</p>
+                        <a href="https://github.com/norfkorean/react-rock-paper-scissors" target="_blank">GitHub</a>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </div>
+    )
 }
